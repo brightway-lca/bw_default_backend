b69d6eff48402ad8606598a939a19bcb5c093106
Chris Mutel <cmutel@gmail.com>
2019-10-16 19:44:24 +0200

    Autohash package files on save

--- bw_default_backend/schema/processed.py
+++ bw_default_backend/schema/processed.py
@@ -1,13 +1,19 @@
 from .geo import Geocollection
 from .lci import Collection
 from .lcia import Method
-from peewee import ForeignKeyField, DateTimeField, Model, SQL
+from brightway_projects.filesystem import md5
 from brightway_projects.peewee import PathField
+from peewee import ForeignKeyField, DateTimeField, Model, SQL, TextField
 
 
 class CalculationPackage(Model):
     filepath = PathField()
+    hash = TextField()
     modified = DateTimeField(constraints=[SQL("DEFAULT CURRENT_TIMESTAMP")])
     geocollection = ForeignKeyField(Geocollection, null=True)
     collection = ForeignKeyField(Collection, null=True)
     method = ForeignKeyField(Method, null=True)
+
+    def save(self, *args, **kwargs):
+        self.hash = md5(self.filepath)
+        super().save(*args, **kwargs)
